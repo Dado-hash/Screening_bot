e21f77a30e3902508b5a0dc266eb1fc4505ec935
Davide <davidegalbiati29@gmail.com>
2022-07-29 11:01:40 +0200

    Adding some initial files

---
+++ Binance API/get_staking_data.py
@@ -0,0 +1,115 @@
+import time
+import json
+import hmac
+import hashlib
+import requests
+from urllib.parse import urljoin, urlencode
+import api_keys
+import api_keys_arcadia
+import pandas as pd
+
+BASE_URL = 'https://api.binance.com'
+
+headers = {
+    'X-MBX-APIKEY': api_keys_arcadia.API_KEY
+}
+
+class BinanceException(Exception):
+    def __init__(self, status_code, data):
+
+        self.status_code = status_code
+        if data:
+            self.code = data['code']
+            self.msg = data['msg']
+        else:
+            self.code = None
+            self.msg = None
+        message = f"{status_code} [{self.code}] {self.msg}"
+
+        # Python 2.x
+        # super(BinanceException, self).__init__(message)
+        super().__init__(message)
+
+old_df = pd.read_csv('old_esempio')
+old_df.set_index('asset', inplace = True)
+PATH = '/sapi/v1/staking/position'
+timestamp = int(time.time() * 1000)
+params = {
+    'product': 'STAKING',
+    'timestamp': timestamp,
+    'recvWindow': 60000
+}
+query_string = urlencode(params)
+params['signature'] = hmac.new(api_keys_arcadia.SECRET.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
+url = urljoin(BASE_URL, PATH)
+r = requests.get(url, headers=headers, params=params)
+if r.status_code == 200:
+    data = r.json()
+    print(json.dumps(data, indent=2))
+else:
+    raise BinanceException(status_code=r.status_code, data=r.json())
+df = pd.DataFrame(data)
+df['purchaseTime'] = pd.to_datetime(df['purchaseTime']/1000, unit = 's')
+df['interestEndDate'] = pd.to_datetime(df['interestEndDate']/1000, unit = 's')
+df['deliverDate'] = pd.to_datetime(df['deliverDate']/1000, unit = 's')
+numeric_columns = ['amount', 'rewardAmt', 'nextInterestPay', 'redeemAmountEarly', 'apy']
+for c in numeric_columns:
+    df[c] = df[c].str.replace(r'.', ',')
+df.set_index('asset', inplace = True) 
+#print(df)
+#df.to_csv('esempio')
+old_df = pd.concat([old_df, df])
+print(old_df)
+old_df.to_csv('old_esempio')
+
+'''PATH_SUB = '/sapi/v3/sub-account/assets'
+timestamp = int(time.time() * 1000)
+params = {
+    'email': '[EMAIL DEL SUBACCOUNT]',
+    'timestamp': timestamp
+}
+query_string = urlencode(params)
+params['signature'] = hmac.new(api_keys.SECRET.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
+url = urljoin(BASE_URL, PATH_SUB)
+r = requests.get(url, headers=headers, params=params)
+if r.status_code == 200:
+    data = r.json()
+    print(json.dumps(data, indent=2))
+else:
+    raise BinanceException(status_code=r.status_code, data=r.json())'''
+
+
+
+'''PATH_SNAPSHOT = '/sapi/v1/accountSnapshot'
+timestamp = int(time.time() * 1000)
+params = {
+    'type': 'SPOT',
+    'timestamp': timestamp,
+    'recvWindow': 60000
+}
+query_string = urlencode(params)
+params['signature'] = hmac.new(api_keys_arcadia.SECRET.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
+url = urljoin(BASE_URL, PATH_SNAPSHOT)
+r = requests.get(url, headers=headers, params=params)
+if r.status_code == 200:
+    data = r.json()
+    print(json.dumps(data, indent=2))
+else:
+    raise BinanceException(status_code=r.status_code, data=r.json())'''
+
+
+'''PATH_FLEXIBLE = '/sapi/v1/lending/daily/token/position'
+timestamp = int(time.time() * 1000)
+params = {
+    'timestamp': timestamp,
+    'recvWindow': 60000
+}
+query_string = urlencode(params)
+params['signature'] = hmac.new(api_keys_arcadia.SECRET.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
+url = urljoin(BASE_URL, PATH_FLEXIBLE)
+r = requests.get(url, headers=headers, params=params)
+if r.status_code == 200:
+    data = r.json()
+    print(json.dumps(data, indent=2))
+else:
+    raise BinanceException(status_code=r.status_code, data=r.json())'''
---
+++ Coingecko API/create files.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import datetime as dt
+import time as t
+import plotly.graph_objects as go
+from plotly.offline import plot
+from pycoingecko import CoinGeckoAPI
+
+cg = CoinGeckoAPI()
+
+cg.ping()
+
+#creo 2 file per avere l'id di 1000 coins
+list_df = []
+for num in range(6):
+    if(num!=0):
+        complexPriceRequest = cg.get_coins_markets(vs_currency = 'btc', order = 'market_cap_desc', per_page = 100, page = num, price_change_percentage = '24h')
+        list_df.append(pd.DataFrame(complexPriceRequest))
+df = pd.concat(list_df)
+list_columns = ['id', 'name', 'current_price', 'market_cap', 'high_24h', 'low_24h', 'price_change_percentage_24h']
+df = df[list_columns]   
+df.set_index("id", inplace = True)
+print(df)
+df.to_csv("idcoins")
+
+list_df = []
+for num in range(11):
+    if(num>5):
+        complexPriceRequest = cg.get_coins_markets(vs_currency = 'btc', order = 'market_cap_desc', per_page = 100, page = num, price_change_percentage = '24h')
+        list_df.append(pd.DataFrame(complexPriceRequest))
+df = pd.concat(list_df)
+list_columns = ['id', 'name', 'current_price', 'market_cap', 'high_24h', 'low_24h', 'price_change_percentage_24h']
+df = df[list_columns]   
+df.set_index("id", inplace = True)
+print(df)
+df.to_csv("idcoins1")
+
+
+#creo il file con i dati aggiornati
+df = pd.read_csv("idcoins")
+coins_id_list = df["id"].tolist()
+complexPriceRequest = cg.get_coins_markets(vs_currency = 'btc', ids = coins_id_list)
+df = pd.DataFrame(complexPriceRequest)
+list_columns = ['id', 'name', 'current_price', 'market_cap', 'high_24h', 'low_24h', 'price_change_percentage_24h']
+df = df[list_columns]   
+df.set_index("id", inplace = True)
+
+df1 = pd.read_csv("idcoins1")
+coins_id_list = df1["id"].tolist()
+complexPriceRequest = cg.get_coins_markets(vs_currency = 'btc', ids = coins_id_list)
+df1 = pd.DataFrame(complexPriceRequest)
+list_columns = ['id', 'name', 'current_price', 'high_24h', 'low_24h', 'price_change_percentage_24h']
+df1 = df1[list_columns]   
+df1.set_index("id", inplace = True)
+df = pd.concat([df, df1])
+print(df)
+df.to_csv("df_principale")
+
+
+#creo il file per avere i prezzi attuali
+df_principale = pd.read_csv("df_principale")
+list_columns = ['id', 'current_price']
+df_current_price = df_principale[list_columns]
+df_current_price.set_index('id', inplace = True)
+df_current_price.to_csv("current_price")
+print(df_current_price)
+
+#creo il file per avere le variazioni gironaliere
+df_principale = pd.read_csv("df_principale")
+list_columns = ['id', 'price_change_percentage_24h']
+df_24h_change = df_principale[list_columns]
+df_24h_change.set_index('id', inplace = True)
+df_24h_change.to_csv("price_change_percentage_24h")
+print(df_24h_change)
+
+#creo il file per avere la volaitilità
+df_principale = pd.read_csv("df_principale")
+df_principale['volatility'] = (df_principale['high_24h'] - df_principale['low_24h'])/df_principale['low_24h']
+list_columns = ['id', 'volatility']
+volatility = df_principale[list_columns]
+volatility.set_index('id', inplace = True)
+volatility.to_csv("volatility")
+print(volatility)
