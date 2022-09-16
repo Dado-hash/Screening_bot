98e0d0978845a60a9836e30486f28957d42255b9
Davide <davidegalbiati29@gmail.com>
2022-09-16 21:22:24 +0200

    Upgraded screening coins to V2

--- Bot_screening/screening_coins_V2.py
+++ Bot_screening/screening_coins_V2.py
@@ -1,25 +1,27 @@
 import pandas as pd
 
-number_coins = int(input("Quante coin ti servono?\n"))
-if(number_coins > 100):
-    range1 = (number_coins//200) + 1
-    range2 = (number_coins//100) + 1
-    print(range1)
-    print(range2)
-
-direction = input ("In che direzione voui calcolare i cumulativi?\n"
+direction = input("In che direzione voui calcolare i cumulativi?\n"
                     "0 -> Da oggi andando indietro\n"
                     "1 -> Viceversa\n")
 
-cumulatives = []
+if(not direction):
+    start = input("Da che giorno vuoi partire?\n")
+
 cumulatives = input("Inserisci il numero di giorni dei cumulativi che ti servono\n")
+cumulatives = cumulatives.split()
+for i in range(len(cumulatives)):
+    cumulatives[i] = int(cumulatives[i])
 
 df_principale = pd.read_excel('storico.xlsx')
+df_principale.set_index('day', inplace = True)
 
 #creo i dataframe con le classifiche incrementali
 leaderboard = []
 for num in cumulatives:
-    df_24h_sum = df_24h.iloc[(31-num) : 31].sum()
+    pd.set_option('display.float_format', lambda x: '%.5f' % x)
+    df_24h_sum = df_principale.T
+    df_24h_sum = (df_24h_sum.iloc[:, len(df_24h_sum.columns)-1] - df_24h_sum.iloc[:, len(df_24h_sum.columns)-num]) / df_24h_sum.iloc[:, len(df_24h_sum.columns)-num]
+    print(df_24h_sum)
     df_24h_sum = df_24h_sum.sort_values(ascending = False)
     df_24h_sum = df_24h_sum.to_frame()
     df = df_24h_sum.copy()[[]]
@@ -31,23 +33,22 @@ for num in cumulatives:
     df_24h_sum = df_24h_sum.merge(ranking, on = 'Cumulative')
     df_24h_sum.index = df.index
     leaderboard.append(df_24h_sum)
+
 with pd.ExcelWriter('leaderboards.xlsx') as writer:  
-    df_principale_24h.to_excel(writer, sheet_name = '24h_change')
-    counter = 1
+    counter = 0
     for df in leaderboard:
-        df.to_excel(writer, sheet_name = str(counter) + 'd')
+        df.to_excel(writer, sheet_name = str(cumulatives[counter]) + 'd')
         counter += 1
 
-first = pd.read_excel('leaderboards.xlsx', sheet_name = '1d')
+first = pd.read_excel('leaderboards.xlsx', sheet_name = str(cumulatives[0]) + 'd')
 first.columns = ['Coin', 'Cumulative', 'Rank']
 first.drop('Rank', inplace = True, axis = 1)
 first.set_index('Coin', inplace = True)
-df_principale_24h = pd.read_excel('leaderboards.xlsx', sheet_name = '24h_change')
 
 leaderboard = []
-for num in range(1, 30):
-    first_df = pd.read_excel('leaderboards.xlsx', sheet_name = str(num) + 'd')
-    second_df = pd.read_excel('leaderboards.xlsx', sheet_name = str(num+1) + 'd')
+for num in range(len(cumulatives)-1):
+    first_df = pd.read_excel('leaderboards.xlsx', sheet_name = str(cumulatives[num]) + 'd')
+    second_df = pd.read_excel('leaderboards.xlsx', sheet_name = str(cumulatives[num+1]) + 'd')
     first_df.columns = ['Coin', 'Cumulative', 'Rank1']
     second_df.columns = ['Coin', 'Cumulative', 'Rank2']
     first_df.drop('Cumulative', inplace = True, axis = 1)
@@ -57,13 +58,12 @@ for num in range(1, 30):
     df.set_index('Coin', inplace = True)
     leaderboard.append(df)
 
-with pd.ExcelWriter('leaderboards.xlsx') as writer:  
-    df_principale_24h.to_excel(writer, sheet_name = '24h_change')
-    counter = 1
+with pd.ExcelWriter('leaderboards.xlsx') as writer:
+    counter = 0
     for df in leaderboard:
         if(counter == 1):
-            first.to_excel(writer, sheet_name = '1d')
+            first.to_excel(writer, sheet_name = str(cumulatives[counter]) + 'd')
             counter += 1
         else:
-            df.to_excel(writer, sheet_name = str(counter) + 'd')
+            df.to_excel(writer, sheet_name = str(cumulatives[counter]) + 'd')
             counter += 1
