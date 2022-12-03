3ac8a2f21338ddcd8c8c77a67d41c8cd07ee3214
Davide <davidegalbiati29@gmail.com>
2022-12-03 12:03:48 +0100

    Fixed volatility

--- Bot_screening/volatility.py
+++ Bot_screening/volatility.py
@@ -17,28 +17,28 @@ closes = pd.read_excel('closes.xlsx', index_col=0, engine='openpyxl')
 coins = highs.columns
 
 for y, count in enumerate(periods):
-    file = open('volatility_' + str(count) + '.csv', 'w')
-    writer = csv.writer(file)
+    dict_val = {}
     for i, val in enumerate(coins):
         if val != 'Close time':
             df_temp = highs[['Close time', val]]
             df_temp.columns = ['Close time', 'high']
             df_temp['low'] = lows[val]
             df_temp['closes'] = closes[val]
-            df_temp['mean'] = df_temp['closes'].astype(float).rolling(count).mean()  #usare media chiusure ultimi count giorni
             df_temp = df_temp.tail(count)
-            df_temp['waste high'] = np.where(df_temp['high'] - df_temp['mean'] >= 0,
-                                             (df_temp['high'] - df_temp['mean']) / df_temp['mean'],
-                                             (df_temp['high'] - df_temp['mean']) / df_temp['high'] * (-1))
-            df_temp['waste low'] = np.where(df_temp['low'] - df_temp['mean'] >= 0,
-                                            (df_temp['low'] - df_temp['mean']) / df_temp['mean'],
-                                            (df_temp['low'] - df_temp['mean']) / df_temp['low'] * (-1))
+            last_closes = df_temp['closes'].tolist()
+            mean = sum(last_closes) / count
+            df_temp['waste high'] = np.where(df_temp['high'] - mean >= 0,
+                                             (df_temp['high'] - mean) / mean,
+                                             (df_temp['high'] - mean) / df_temp['high'] * (-1))
+            df_temp['waste low'] = np.where(df_temp['low'] - mean >= 0,
+                                            (df_temp['low'] - mean) / mean,
+                                            (df_temp['low'] - mean) / df_temp['low'] * (-1))
             df_temp['waste'] = np.where(df_temp['waste high'] >= df_temp['waste low'], df_temp['waste high'],
                                         df_temp['waste low'])
-            df_temp.drop(['waste high', 'waste low', 'mean'], inplace=True, axis=1)
+            df_temp.drop(['waste high', 'waste low'], inplace=True, axis=1)
             df_temp['waste'] = df_temp['waste'] ** 2
             waste = df_temp['waste'].tolist()
             standard_deviation = (sum(waste) / count) ** (1 / 2) * 100
-            print(val + ' -> ' + str(standard_deviation) + '\n')
-            writer.writerow(val + ' -> ' + str(standard_deviation))
-    file.close()
+            dict_val[val] = standard_deviation
+    df_sd = pd.Series(dict_val, name='Standard deviation')
+    df_sd.to_excel('volatility_' + str(count) + '.xlsx')
